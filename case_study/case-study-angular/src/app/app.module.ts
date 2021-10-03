aaaeccd5ebb9b34f5cc0e48292dc1b9dcb65c2c0
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-10-03 22:58:41 +0700

    initial commit

---
+++ case_study/case-study-angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './component/employee/create-employee/create-employee.component';
+import { ListEmployeeComponent } from './component/employee/list-employee/list-employee.component';
+import { EditEmployeeComponent } from './component/employee/edit-employee/edit-employee.component';
+import { ListServiceComponent } from './component/service/list-service/list-service.component';
+import { CreateServiceComponent } from './component/service/create-service/create-service.component';
+import { EditServiceComponent } from './component/service/edit-service/edit-service.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeeComponent,
+    EditEmployeeComponent,
+    ListServiceComponent,
+    CreateServiceComponent,
+    EditServiceComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
